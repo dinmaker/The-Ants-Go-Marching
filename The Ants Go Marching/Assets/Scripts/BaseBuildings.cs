4fd10f7691703dd71a6c650a1516bf415e3a044c
dinmaker <37370379+dinmaker@users.noreply.github.com>
2020-03-03 14:00:47 +0000

    script updates

---
+++ The Ants Go Marching/Assets/Scripts/BaseBuildings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBuildings : MonoBehaviour
+{
+    public GameObject controllerObject;
+    public GameController controller;
+
+    public int farms;
+    public int nursery;
+    public int barracks;
+    public int residential;
+    public int foodStore;
+    public int waterStore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerObject = GameObject.Find("GameController");
+        controller = controllerObject.GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- The Ants Go Marching/Assets/Scripts/GameController.cs
+++ The Ants Go Marching/Assets/Scripts/GameController.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    bool spawnAntType;
-    int workerAnts;
-    int maxWorkers;
-    int soldierAnts;
-    int maxSoldiers;
+    public bool spawnAntType;
+    public int workerAnts;
+    public int maxWorkers;
+    public int soldierAnts;
+    public int maxSoldiers;
+    public int food;
+    public int maxFood;
+    public int water;
+    public int maxWater;
+    public int consumption;
+    public int spawning;
 
-
-    int food;
-    int maxFood;
-    int water;
-    int maxWater;
-
-    int consumption;
-
+    public GameObject playerHive;
+    public BaseBuildings playerbuildings;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine (Cycle());
+        playerHive = GameObject.Find("PlayerHive");
+        playerbuildings = playerHive.GetComponent<BaseBuildings>();
     }
 
     // Update is called once per frame
@@ -35,6 +37,165 @@ public class GameController : MonoBehaviour
         if (soldierAnts >= maxSoldiers)
         {
             soldierAnts = maxSoldiers;
+        }
+
+        consumption = workerAnts + soldierAnts;
+        switch (playerbuildings.farms)
+        {
+            case 5:
+                consumption = (workerAnts + soldierAnts) - 100;
+                break;
+            case 4:
+                consumption = (workerAnts + soldierAnts) - 80;
+                break;
+            case 3:
+                consumption = (workerAnts + soldierAnts) - 60;
+                break;
+            case 2:
+                consumption = (workerAnts + soldierAnts) - 40;
+                break;
+            case 1:
+                consumption = (workerAnts + soldierAnts) - 20;
+                break;
+            default:
+                consumption = workerAnts + soldierAnts;
+                break;
+        }
+
+        if(consumption < 0)
+        {
+            consumption = 0;
+        }
+
+        switch (playerbuildings.residential)
+        {
+            case 5:
+                maxWorkers = 100;
+                break;
+            case 4:
+                maxWorkers = 90;
+                break;
+            case 3:
+                maxWorkers = 80;
+                break;
+            case 2:
+                maxWorkers = 70;
+                break;
+            case 1:
+                maxWorkers = 60;
+                break;
+            default:
+                maxWorkers = 50;
+                break;
+        }
+
+        switch (playerbuildings.barracks)
+        {
+            case 5:
+                maxSoldiers = 100;
+                break;
+            case 4:
+                maxSoldiers = 90;
+                break;
+            case 3:
+                maxSoldiers = 80;
+                break;
+            case 2:
+                maxSoldiers = 70;
+                break;
+            case 1:
+                maxSoldiers = 60;
+                break;
+            default:
+                maxSoldiers = 50;
+                break;
+        }
+
+        switch (playerbuildings.nursery)
+        {
+            case 5:
+                spawning = 6;
+                break;
+            case 4:
+                spawning = 5;
+                break;
+            case 3:
+                spawning = 4;
+                break;
+            case 2:
+                spawning = 3;
+                break;
+            case 1:
+                spawning = 2;
+                break;
+            default:
+                spawning = 1;
+                break;
+        }
+
+        switch (playerbuildings.foodStore)
+        {
+            case 5:
+                maxFood = 200;
+                break;
+            case 4:
+                maxFood = 180;
+                break;
+            case 3:
+                maxFood = 160;
+                break;
+            case 2:
+                maxFood = 140;
+                break;
+            case 1:
+                maxFood = 120;
+                break;
+            default:
+                maxFood = 100;
+                break;
+        }
+
+        switch (playerbuildings.waterStore)
+        {
+            case 5:
+                maxWater = 200;
+                break;
+            case 4:
+                maxWater = 180;
+                break;
+            case 3:
+                maxWater = 160;
+                break;
+            case 2:
+                maxWater = 140;
+                break;
+            case 1:
+                maxWater = 120;
+                break;
+            default:
+                maxWater = 100;
+                break;
+        }
+
+    }
+
+
+    IEnumerator Cycle()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(10.0f);
+            Debug.Log("Cycle Start");
+            if (spawnAntType == false)
+            {
+                workerAnts += spawning;
+            }
+            else if (spawnAntType == true)
+            {
+                soldierAnts += spawning;
+            }
+            food -= consumption;
+            water -= consumption;
         }
     }
 }
