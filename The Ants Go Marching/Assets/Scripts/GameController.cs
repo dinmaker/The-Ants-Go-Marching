8eea6fc31969dfc9743822b2e6792c4cf913d581
dinmaker <37370379+dinmaker@users.noreply.github.com>
2020-03-03 11:05:38 +0000

    adding scripts

---
+++ The Ants Go Marching/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    bool spawnAntType;
+    int workerAnts;
+    int maxWorkers;
+    int soldierAnts;
+    int maxSoldiers;
+
+
+    int food;
+    int maxFood;
+    int water;
+    int maxWater;
+
+    int consumption;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (workerAnts >= maxWorkers)
+        {
+            workerAnts = maxWorkers;
+        }
+        if (soldierAnts >= maxSoldiers)
+        {
+            soldierAnts = maxSoldiers;
+        }
+    }
+}
